4c5bcf3422ffaf2e6ae277def0b00e817aeb7264
VenturaHaze <ijumperer@gmail.com>
2023-10-06 21:40:25 -0700

    Benni

--- Custom2DGame/environment.py
+++ Custom2DGame/environment.py
@@ -8,12 +8,14 @@ import pygame
 import time
 
 class PlainFieldEnv(gymnasium.Env):
-    def __init__(self, field_size=8):
+    def __init__(self, field_size=8, vision_size=5):
         super(PlainFieldEnv, self).__init__()
 
         # Field dimensions
         self.num_rows = field_size
         self.num_cols = field_size
+
+        self.vision_size = vision_size
 
         # Randomize start and goal positions
         self.start_pos = self._random_position()
@@ -37,6 +39,14 @@ class PlainFieldEnv(gymnasium.Env):
 
         # intially, the display is not initialized
         self.display_initialized = False
+
+        # Reward dictionary
+        self.rewards = {
+            'move': -1.0 / field_size,
+            'invalid_move': -2.0 / field_size,
+            'goal_reached': 1.0,
+            'truncated': -1.0
+        }
 
     def reset(self, seed=None, options=None):
         if seed is not None:
@@ -67,18 +77,18 @@ class PlainFieldEnv(gymnasium.Env):
         self.facing_direction = self.facing_directions[action]
 
         # cost of moving
-        reward = -1.0/self.num_rows
+        reward = self.rewards['move']
 
         # Check boundaries
         if not self._is_valid_position(tuple(new_pos)):
-            reward -= 1.0/self.num_rows  # Negative reward for an invalid move
+            reward += self.rewards['invalid_move']  # Note: we're adding because the reward is negative
             self.current_pos = original_pos  # Reset to original position
         else:
             self.current_pos = tuple(new_pos)
 
         # Goal check
         if self.current_pos == self.goal_pos:
-            reward += 1.0  # Large reward for reaching the goal
+            reward += self.rewards['goal_reached']
             terminated = True
         else:
             terminated = False
@@ -88,7 +98,7 @@ class PlainFieldEnv(gymnasium.Env):
         truncated = self.steps_since_reset > (self.num_rows ** 2)
 
         if truncated:
-            reward -= 1.0
+            reward += self.rewards['truncated']
 
         return self._get_vision(), reward, terminated, truncated, {}
 
@@ -96,17 +106,19 @@ class PlainFieldEnv(gymnasium.Env):
         return 1 <= pos[0] <= self.num_rows and 1 <= pos[1] <= self.num_cols
 
     def _get_vision(self):
-        vision = np.zeros((3, 3), dtype=np.int8)
-        offsets = [(-1, -1), (-1, 0), (-1, 1),
-                   (0, -1), (0, 0), (0, 1),
-                   (1, -1), (1, 0), (1, 1)]
+        half_vision = self.vision_size // 2
+        vision = np.zeros((self.vision_size, self.vision_size), dtype=np.int8)
+        vision_coordinates = []  # To store coordinates of cells in the agent's vision
 
-        for i, (dx, dy) in enumerate(offsets):
-            x, y = self.current_pos[0] + dx, self.current_pos[1] + dy
-            if (x, y) == self.goal_pos:
-                vision[i // 3, i % 3] = 1  # 1 denotes the goal
+        for dx in range(-half_vision, half_vision + 1):
+            for dy in range(-half_vision, half_vision + 1):
+                x, y = self.current_pos[0] + dx, self.current_pos[1] + dy
+                vision_coordinates.append((x, y))
+                if 0 <= x <= self.num_rows and 0 <= y <= self.num_cols:  # Check if it's within the grid
+                    if (x, y) == self.goal_pos:
+                        vision[dx + half_vision, dy + half_vision] = 1  # 1 denotes the goal
 
-        return vision
+        return vision, vision_coordinates
 
     def _setup_display(self):
         pygame.init()
@@ -126,7 +138,6 @@ class PlainFieldEnv(gymnasium.Env):
         pygame.draw.rect(self.screen, (0, 0, 0), (0, 0, self.num_cols * self.cell_size, self.num_rows * self.cell_size),
                          line_width)
 
-        # Draw start and goal positions
         cell_margin = 3
         for row in range(1, self.num_rows + 1):
             for col in range(1, self.num_cols + 1):
@@ -134,6 +145,7 @@ class PlainFieldEnv(gymnasium.Env):
                 cell_top = (row - 1) * self.cell_size
                 cell = (row, col)
 
+                # Draw start and goal positions
                 if cell == self.start_pos:
                     pygame.draw.rect(self.screen, (0, 255, 0), (cell_left + cell_margin, cell_top + cell_margin,
                                                                 self.cell_size - cell_margin,
@@ -143,29 +155,43 @@ class PlainFieldEnv(gymnasium.Env):
                                                                 self.cell_size - cell_margin,
                                                                 self.cell_size - cell_margin), 3)
 
-                # Draw agent
-                if cell == self.current_pos:
-                    center_x = int(cell_left + self.cell_size / 2)
-                    center_y = int(cell_top + self.cell_size / 2)
-                    half_size = int(self.cell_size / 3)
+        # Draw agent's vision using the stored vision matrix
+        vision_surface = pygame.Surface((self.cell_size, self.cell_size), pygame.SRCALPHA)
+        vision_color = (255, 165, 0, 90)  # 30% transparent orange
+        vision_surface.fill(vision_color)
 
-                    if self.facing_direction == 'N':
-                        triangle_vertices = [(center_x, center_y - half_size),
-                                             (center_x - half_size, center_y + half_size),
-                                             (center_x + half_size, center_y + half_size)]
-                    elif self.facing_direction == 'E':
-                        triangle_vertices = [(center_x + half_size, center_y),
-                                             (center_x - half_size, center_y - half_size),
-                                             (center_x - half_size, center_y + half_size)]
-                    elif self.facing_direction == 'S':
-                        triangle_vertices = [(center_x, center_y + half_size),
-                                             (center_x - half_size, center_y - half_size),
-                                             (center_x + half_size, center_y - half_size)]
-                    else:  # 'W'
-                        triangle_vertices = [(center_x - half_size, center_y),
-                                             (center_x + half_size, center_y - half_size),
-                                             (center_x + half_size, center_y + half_size)]
+        _, vision_coordinates = self._get_vision()  # We only need the coordinates here
 
-                    pygame.draw.polygon(self.screen, (0, 0, 255), triangle_vertices)
+        for x, y in vision_coordinates:
+            if 1 <= x <= self.num_rows and 1 <= y <= self.num_cols:  # Ensure we are in the boundary
+                cell_left = (y - 1) * self.cell_size
+                cell_top = (x - 1) * self.cell_size
+                self.screen.blit(vision_surface, (cell_left, cell_top))
+
+        # Draw agent at its current position
+        cell_left = (self.current_pos[1] - 1) * self.cell_size
+        cell_top = (self.current_pos[0] - 1) * self.cell_size
+        center_x = int(cell_left + self.cell_size / 2)
+        center_y = int(cell_top + self.cell_size / 2)
+        half_size = int(self.cell_size / 3)
+
+        if self.facing_direction == 'N':
+            triangle_vertices = [(center_x, center_y - half_size),
+                                 (center_x - half_size, center_y + half_size),
+                                 (center_x + half_size, center_y + half_size)]
+        elif self.facing_direction == 'E':
+            triangle_vertices = [(center_x + half_size, center_y),
+                                 (center_x - half_size, center_y - half_size),
+                                 (center_x - half_size, center_y + half_size)]
+        elif self.facing_direction == 'S':
+            triangle_vertices = [(center_x, center_y + half_size),
+                                 (center_x - half_size, center_y - half_size),
+                                 (center_x + half_size, center_y - half_size)]
+        else:  # 'W'
+            triangle_vertices = [(center_x - half_size, center_y),
+                                 (center_x + half_size, center_y - half_size),
+                                 (center_x + half_size, center_y + half_size)]
+
+        pygame.draw.polygon(self.screen, (0, 0, 255), triangle_vertices)
 
         pygame.display.update()

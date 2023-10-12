1d26bd4c96110aebe614ed23089a5f32e25bcf8f
VenturaHaze <ijumperer@gmail.com>
2023-10-11 17:14:54 -0700

    Benni

--- GameTheory/game_env.py
+++ GameTheory/game_env.py
@@ -20,20 +20,33 @@ class GameEnv(gym.Env):
 
         self.renderer = GameRenderer(self.game_modes)  # Pass instantiated GameModes object to GameRenderer
 
+        # reset after each step
         self.agent1_action = None
         self.agent2_action = None
         self.agent1_reward = 0
         self.agent2_reward = 0
 
+        # reset after each episode
         self.agent1_cumulative_reward = 0
         self.agent2_cumulative_reward = 0
         self.agent1_avg_reward = 0
         self.agent2_avg_reward = 0
 
+        # never reset
+        self.agent1_total_cumulative_reward = 0
+        self.agent2_total_cumulative_reward = 0
+        self.agent1_total_avg_reward = 0
+        self.agent2_total_avg_reward = 0
+
         self.should_render = render_mode
         self.max_rounds = max_rounds
         self.current_round = 0
+        self.total_rounds = 0
+        self.episode_counter = 1  # Initialize episode counter
         self.history = deque(maxlen=self.max_rounds)  # To store history of actions
+
+        self.terminated = False
+        self.truncated = False
 
     def reset(self):
         self.agent1_cumulative_reward = 0
@@ -46,6 +59,7 @@ class GameEnv(gym.Env):
 
     def step(self, action_agent1, action_agent2):
         self.current_round += 1
+        self.total_rounds += 1
         self.agent1_action = action_agent1
         self.agent2_action = action_agent2  # This is our opponent for agent1
 
@@ -60,20 +74,32 @@ class GameEnv(gym.Env):
         self.agent1_avg_reward = self.agent1_cumulative_reward / self.current_round
         self.agent2_avg_reward = self.agent2_cumulative_reward / self.current_round
 
-        terminated = self.current_round >= self.max_rounds
-        truncated = False  # This can remain False for now
+        # Update total cumulative rewards
+        self.agent1_total_cumulative_reward += self.agent1_reward
+        self.agent2_total_cumulative_reward += self.agent2_reward
+
+        # Update total average rewards
+        self.agent1_total_avg_reward = self.agent1_total_cumulative_reward / self.total_rounds
+        self.agent2_total_avg_reward = self.agent2_total_cumulative_reward / self.total_rounds
+
+        self.terminated = self.current_round >= self.max_rounds
+        self.truncated = False  # This can remain False for now
 
         # Storing history
         self.history.append((self.agent1_action, self.agent2_action))
 
         # Return formatted for two agents
-        return (self.agent2_action, self.agent1_action), (self.agent1_reward, self.agent2_reward), terminated, truncated, {}
+        return (self.agent2_action, self.agent1_action), (self.agent1_reward, self.agent2_reward), self.terminated, self.truncated, {}
 
     def render(self, mode='human'):
         if self.should_render and mode == 'human':
             self.renderer.render(self.agent1_action, self.agent2_action, self.agent1_reward, self.agent2_reward,
                                  self.agent1_cumulative_reward, self.agent2_cumulative_reward,
-                                 self.agent1_avg_reward, self.agent2_avg_reward)
+                                 self.agent1_avg_reward, self.agent2_avg_reward,
+                                 self.agent1_total_cumulative_reward, self.agent2_total_cumulative_reward,
+                                 self.agent1_total_avg_reward, self.agent2_total_avg_reward,
+                                 self.episode_counter,
+                                 self.current_round)  # Added episode and round counters as parameters
 
     def close(self):
         self.renderer.close()

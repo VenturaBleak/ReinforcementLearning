ffd67b57b07cea0e3552621313e3518c12f0d3d5
VenturaHaze <ijumperer@gmail.com>
2023-10-11 21:37:25 -0700

    Benni

--- GameTheory/game_modes.py
+++ GameTheory/game_modes.py
@@ -24,13 +24,23 @@ class GameModes:
                 "action_names": ["heads", "tails"]
             },
             "prisoners_dilemma": {
-                "payoffs": np.array([(3, 3), (0, 5), (5, 0), (1, 1)]),
+                "payoffs": np.array([[(3, 3), (0, 5)], [(5, 0), (1, 1)]]),
                 "action_names": ["cooperate", "defect"]
             }
         }
 
     def get_payoff(self, action1, action2):
         return self.games[self.game_type]["payoffs"][action1][action2]
+
+    def get_payoff_matrix(self):
+        return self.games[self.game_type]["payoffs"]
+
+    def get_payoff_bounds(self):
+        # Fetching the payoffs matrix for the current game type
+        payoff_matrix = self.games[self.game_type]["payoffs"]
+        min_payoff = np.min(payoff_matrix)
+        max_payoff = np.max(payoff_matrix)
+        return min_payoff, max_payoff
 
     def get_action_names(self):
         return self.games[self.game_type]["action_names"]
--- GameTheory/game_visualize.py
+++ GameTheory/game_visualize.py
@@ -3,17 +3,26 @@ from game_env import GameEnv
 NUM_EPISODES = 5  # Play 5 episodes
 MAX_ROUNDS_PER_EPISODE = 10  # Each episode has 10 rounds
 
-env = GameEnv("chicken", True, max_rounds=MAX_ROUNDS_PER_EPISODE)
+env = GameEnv("prisoners_dilemma", render_mode="human", max_rounds=MAX_ROUNDS_PER_EPISODE)
 
 for _ in range(NUM_EPISODES):
-    observation = env.reset()
+    observations, _ = env.reset()
+    observation_agent1, observation_agent2 = observations
     for _ in range(MAX_ROUNDS_PER_EPISODE):
+        # debug
+        # print("Observation agent 1: ", observation_agent1)
+        # print("Observation agent 2: ", observation_agent2)
+
+        # total_dims = len(observation_agent1)  # Get the observation space for agent 1
+        # print("Total dimensions: ", total_dims)
+
         action_agent1 = env.action_space.sample()
         action_agent2 = env.action_space.sample()
         observations, rewards, terminated, truncated, info = env.step(action_agent1, action_agent2)
         observation_agent1, observation_agent2 = observations
+
         reward_agent1, reward_agent2 = rewards
-        env.render('human')
+        env.render()
         if terminated:
             env.episode_counter += 1  # Increment episode counter after termination
             break
